cf625e03d04a495301d4cf45a09e4590958c4682
Cosmic.id <100512113+cosmic-id@users.noreply.github.com>
2024-01-20 21:55:56 +0700

    Update aurora-v2-lora-p2p-transmitter.ino

--- examples/aurora-v2-lora-p2p-transmitter/aurora-v2-lora-p2p-transmitter.ino
+++ examples/aurora-v2-lora-p2p-transmitter/aurora-v2-lora-p2p-transmitter.ino
@@ -12,9 +12,6 @@
 #define LORA_AURORA_V2_DIO0 27
 #define LORA_AURORA_V2_EN 32
 
-#define LORA_TX_POWER 20
-#define LORA_SPREADING_FACTOR 12
-
 int counter = 0;
 
 void setup() {
@@ -39,9 +36,6 @@ void setup() {
     Serial.println(".");
     delay(500);
   }
-
-  LoRa.setTxPower(LORA_TX_POWER, PA_OUTPUT_PA_BOOST_PIN);
-  LoRa.setSpreadingFactor(LORA_SPREADING_FACTOR);
   
   // Change sync word (0xF3) to match the receiver
   // The sync word assures you don't get LoRa messages from other LoRa transceivers
