d0c4c5f92f0c518743e45b724d47b4d9b33b25e7
Cosmic <cosmic.indonesia01@gmail.com>
2025-07-20 14:56:50 +0700

    fix typos

--- examples/aurora-v3-lora-p2p-receiver/aurora-v3-lora-p2p-receiver.ino
+++ examples/aurora-v3-lora-p2p-receiver/aurora-v3-lora-p2p-receiver.ino
@@ -7,19 +7,16 @@
 #include <LoRa.h>
 
 //define the pins used by the transceiver module
-#define LORA_AURORA_V2_NSS 13
-#define LORA_AURORA_V2_RST 16
-#define LORA_AURORA_V2_DIO0 27
-#define LORA_AURORA_V2_EN 15
-
-#define LORA_TX_POWER 20
-#define LORA_SPREADING_FACTOR 12
+#define LORA_AURORA_V3_NSS 13
+#define LORA_AURORA_V3_RST 16
+#define LORA_AURORA_V3_DIO0 27
+#define LORA_AURORA_V3_EN 15
 
 void setup() {
   // Initiate the LoRa Enable pin
-  pinMode(LORA_AURORA_V2_EN, OUTPUT);
+  pinMode(LORA_AURORA_V3_EN, OUTPUT);
   // LoRa chip is Active Low
-  digitalWrite(LORA_AURORA_V2_EN, LOW);
+  digitalWrite(LORA_AURORA_V3_EN, LOW);
   
   //initialize Serial Monitor
   Serial.begin(115200);
@@ -27,7 +24,7 @@ void setup() {
   Serial.println("LoRa Receiver");
 
   //setup LoRa transceiver module
-  LoRa.setPins(LORA_AURORA_V2_NSS, LORA_AURORA_V2_RST, LORA_AURORA_V2_DIO0);
+  LoRa.setPins(LORA_AURORA_V3_NSS, LORA_AURORA_V3_RST, LORA_AURORA_V3_DIO0);
   
   //replace the LoRa.begin(---E-) argument with your location's frequency 
   //433E6 for Asia
@@ -37,11 +34,8 @@ void setup() {
     Serial.println(".");
     delay(500);
   }
-
-  //LoRa.setTxPower(LORA_TX_POWER);
-  //LoRa.setSpreadingFactor(LORA_SPREADING_FACTOR);
   
-   // Change sync word (0xF3) to match the receiver
+  // Change sync word (0xF3) to match the receiver
   // The sync word assures you don't get LoRa messages from other LoRa transceivers
   // ranges from 0-0xFF
   LoRa.setSyncWord(0xF3);
